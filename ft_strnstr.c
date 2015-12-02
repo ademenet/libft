5957877b2449e9579a800110f673f4fc33e59c2b
Alain DEMENET <ademenet@e3r10p14.42.fr>
2015-12-02 16:35:45 +0100

    corrige le comportement de la fonction: il faut que l'index du char teste prenne en compte la longueur de la deuxieme chaine, s2

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: ademenet <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/11/30 11:10:13 by ademenet          #+#    #+#             */
-/*   Updated: 2015/11/30 11:15:37 by ademenet         ###   ########.fr       */
+/*   Updated: 2015/12/02 16:32:06 by ademenet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,15 +16,19 @@
 char	*ft_strnstr(const char *s1, const char *s2, size_t n)
 {
 	size_t	len;
+	int		i;
 
-	if (!s2)
+	i = 0;
+	if (!*s2)
 		return((char *)s1);
 	len = ft_strlen(s2);
-	while (*s1 && n--)
+	if (n < len)
+		return (NULL);
+	while (s1[i] != '\0' && i <= (n - len))
 	{
-		if (ft_strncmp(s1, s2, len) == 0)
-			return ((char *)s1);
-		s1++;
+		if (ft_strncmp(&s1[i], s2, len) == 0)
+			return ((char *)&s1[i]);
+		i++;
 	}
 	return (NULL);
 }
