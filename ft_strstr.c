f679ddf357c1f98ed5c590eb5d49f8819ab13446
Alain DEMENET <ademenet@e3r10p14.42.fr>
2015-12-02 14:18:37 +0100

    corrige le bug si la chaine s2 est nulle

--- ft_strstr.c
+++ ft_strstr.c
@@ -6,7 +6,7 @@
 /*   By: ademenet <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/11/24 18:49:04 by ademenet          #+#    #+#             */
-/*   Updated: 2015/12/01 17:54:28 by ademenet         ###   ########.fr       */
+/*   Updated: 2015/12/02 14:16:38 by ademenet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,8 +17,8 @@ char	*ft_strstr(const char *s1, const char *s2)
 {
 	size_t		len;
 
-	if (*s1 == '\0' && *s2 == '\0')
-		return (NULL);
+	if (*s2 == '\0')
+		return ((char *)s1);
 	if (!s2)
 		return ((char *)s1);
 	len = ft_strlen(s2);
