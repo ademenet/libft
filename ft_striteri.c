775778c19b66916a88d129f396927d961b4d9833
Alain DEMENET <ademenet@e2r7p14.42.fr>
2015-12-15 17:59:16 +0100

    Ajout de protection des fonctions.

--- ft_striteri.c
+++ ft_striteri.c
@@ -6,7 +6,7 @@
 /*   By: ademenet <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/11/26 09:42:05 by ademenet          #+#    #+#             */
-/*   Updated: 2015/12/02 09:53:50 by ademenet         ###   ########.fr       */
+/*   Updated: 2015/12/15 17:56:19 by ademenet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,9 +15,12 @@ void	ft_striteri(char *s, void (*f)(unsigned int, char *))
 	unsigned long	i;
 
 	i = 0;
-	while (s[i] != '\0')
+	if (s)
 	{
-		(*f)(i, &s[i]);
-		i++;
+		while (s[i] != '\0')
+		{
+			(*f)(i, &s[i]);
+			i++;
+		}
 	}
 }
