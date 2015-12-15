b789355d1ba6186560e99af2aa6642cf27d18c6a
Alain DEMENET <ademenet@e2r7p14.42.fr>
2015-12-15 16:10:35 +0100

    Regle le probleme de '\0' a la fin (du a un decalage d'index) et refactorisation.

--- ft_strdup.c
+++ ft_strdup.c
@@ -6,11 +6,10 @@
 /*   By: ademenet <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/11/23 17:44:06 by ademenet          #+#    #+#             */
-/*   Updated: 2015/11/26 08:30:24 by ademenet         ###   ########.fr       */
+/*   Updated: 2015/12/15 16:06:35 by ademenet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdlib.h>
 #include <string.h>
 #include "libft.h"
 
@@ -18,15 +17,12 @@ char	*ft_strdup(const char *s1)
 {
 	char	*dst;
 	size_t	len;
-	int		i;
 
-	i = 0;
 	len = ft_strlen(s1);
-	dst = malloc(len);
-	while (s1[i] != '\0')
-	{
-		dst[i] = s1[i];
-		i++;
-	}
-	return (&dst[0]);
+	dst = ft_strnew(len);
+	if (!dst)
+		return (NULL);
+	while (len--)
+		dst[len] = s1[len];
+	return (dst);
 }
